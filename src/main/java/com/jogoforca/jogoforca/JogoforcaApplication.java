4f4ea2c59f59f54a4bd69fee047c7a67ab8819cc
Miguel Nunes <miguel.npa@gmail.com>
2023-10-10 15:58:29 -0300

    Projeto criado

---
+++ src/main/java/com/jogoforca/jogoforca/JogoforcaApplication.java
@@ -0,0 +1,13 @@
+package com.jogoforca.jogoforca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoforcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogoforcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jogoforca/jogoforca/JogoforcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jogoforca.jogoforca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogoforcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
